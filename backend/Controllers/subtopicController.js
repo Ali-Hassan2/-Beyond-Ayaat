8aa23738e7c44294dab28f607b4b7f4c99e214fb
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-07-29 18:42:45 +0500

    Subtopics and Cloudiary Setup successfull

---
+++ backend/Controllers/subtopicController.js
@@ -0,0 +1,75 @@
+const Topic = require("../Models/topic-model");
+const Subtopic = require("../Models/subtopics-model");
+const subtopicvalidation = require("../Validations/subtopicvalidation");
+const cloudinary = require("cloudinary").v2;
+const sendResponse = require("../Utils/send-response");
+const creatingsubtopic = async (req, res) => {
+  const subtopicval = subtopicvalidation.safeParse(req.body);
+  if (!subtopicval.success) {
+    return sendResponse(
+      res,
+      400,
+      false,
+      "Please validate the input",
+      null,
+      subtopicval.error.issues.map((err) => err?.message)
+    );
+  }
+
+  try {
+    const title = req.body?.title;
+    const description = req.body?.description;
+    const topic_id = req.body?.topic_id;
+    const summary = req.body?.summary;
+    const image = req.files?.image;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return sendResponse(res, 400, false, "Sorry no images found");
+    }
+
+    const allowed_formats = ["image/png", "image/jpeg"];
+    if (!allowed_formats.includes(image.mimetype)) {
+      return res.status(400).send({
+        success: false,
+        message: "Sorry this format is not allowed for images.",
+      });
+    }
+
+    const uploadResult = cloudinary.uploader.upload(image.tempFilePath);
+    if (!uploadResult || uploadResult.error) {
+      return res.status(400).json({
+        success: false,
+        message: "Sorry cannot upload images",
+      });
+    }
+
+    if (!title || !description || !topic_id || !summary) {
+      return sendResponse(res, 400, false, "Please complete the input");
+    }
+
+    const adminid = req.adminid;
+    if (!adminid) {
+      return sendResponse(res, 400, false, "Only admin can create subtopics");
+    }
+
+    const topic = await Topic.findById(topic_id);
+    if (!topic) sendResponse(res, 400, false, "Topic not found!");
+
+    const payload = {
+      title: title,
+      description: description,
+      topic_id: topic_id,
+      summary: summary,
+    };
+    const newSubTopic = new Subtopic(payload);
+    await newSubTopic.save();
+    sendResponse(res, 200, true, "Subtopic created successfully", newSubTopic);
+  } catch (error) {
+    console.log("There is an error while creating the subtopic", error);
+    sendResponse(res, 500, false, [error?.message]);
+  }
+};
+
+const gettingsubtopics = async (req, res) => {};
+
+module.exports = { gettingsubtopics, creatingsubtopic };
--- backend/Utils/googleapiConfig.js
+++ backend/Utils/googleapiConfig.js
@@ -1,9 +1,11 @@
 const { google } = require("googleapis");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const REDIRECT_URL = process.env.REDIRECT_URL;
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  "http://localhost:5173" 
+  REDIRECT_URL
 );
 
 module.exports = oauth2Client;

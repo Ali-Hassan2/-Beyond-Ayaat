7203d4b021c28afa41f3987a6436f28d85ee3cee
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-10 01:08:10 +0500

    some content added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import {Home} from './Pages/Home';
-import {Signin} from './Pages/Signin';
-import {Signup} from './Pages/Signup';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Signin } from "./Pages/Signin";
+import { Signup } from "./Pages/Signup";
+import "./App.css";
 
 
 
 
 const router = createBrowserRouter([
-  { path: '/',
-   element: <Home /> },
-
-   { path: '/signup',
-    element: < Signup /> },
-   
-
-
-  { path: '/signin', 
-   element: <Signin /> },
-   
+  { path: "/", element: <Home /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/signin", element: <Signin /> },
 ]);
 
 function App() {

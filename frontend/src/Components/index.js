0aa1586e2453dbc1d844da58802daf63c8a7621f
Ali Hassan Sadiq <alihassansadiq9@gmail.com>
2025-08-21 23:08:19 +0500

    admin signin

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -5,3 +5,4 @@ export { Navbar } from "./HomeComponents/Navbar";
 export { Sign_in } from "./SigninComponent/Sign_in";
 export { Sign_up } from "./SignupComponent/Sign_up";
 export { AdminSign_up } from "./AdminComponents/AdminSignup/AdminSign_up";
+export { AdminSign_in } from "./AdminComponents/AdminSignin/AdminSign_in";
